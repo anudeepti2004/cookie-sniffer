d259f18f46026ea3f8fcb6adef1558590f905645
nandukalidindi <nandukalidindi@gmail.com>
2017-08-22 03:52:44 -0400

    Prepare serializable response to send to ACHE and set a timeout of 10 seconds to wait for cookies to be reset

--- background.js
+++ background.js
@@ -14,47 +14,71 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
       attempts = 0;
 
   buildCookie(request.domain).then(function(response) {
-    initialCookie = response;
-    intervalBuiltCookie = response;
+    initialCookie = response.stringifiedCookie;
+    intervalBuiltCookie = response.stringifiedCookie;
 
     if(request.method === 'startTailing') {
-      var interval = setInterval(function() {
-        if(attempts > 10)
-          clearInterval(interval);
+      setTimeout(function() {
+        var interval = setInterval(function() {
+          if(attempts > 10)
+            clearInterval(interval);
 
-        if(initialCookie !== intervalBuiltCookie) {
-          //send response to ACHE
-          alert(intervalBuiltCookie);
-          clearInterval(interval);
-        }
-        attempts++;
-        buildCookie(request.domain).then(function(response) {
-          intervalBuiltCookie = response;
-        });
-      }, 2000)
+          if(initialCookie !== intervalBuiltCookie) {
+            //send response to ACHE which is intervalBuiltCookie.serializableCookie
+            alert(intervalBuiltCookie);
+            clearInterval(interval);
+          }
+          attempts++;
+          buildCookie(request.domain).then(function(response) {
+            intervalBuiltCookie = response.stringifiedCookie;
+          });
+        }, 2000);
+      }, 10000);
     } else if (request.method === 'cookieMessenger') {
-      // send response to ache with
+      //send response to ACHE which is intervalBuiltCookie.serializableCookie
     }
   });
 });
 
 function buildCookie(domainName) {
-  var fullCookie = [];
+  var fullCookie = [],
+      serializableCookie = [];
 
   return new Promise(function(success, error) {
     try {
       chrome.cookies.getAll({domain: domainName}, function(response) {
         response.forEach(function(cookie) {
           fullCookie.push(cookie.name + "=" + cookie.value);
+          serializableCookie.push(mutateCookieResponse(cookie));
         })
 
-        success(fullCookie.join("; "));
+        success({stringifiedCookie: fullCookie.join("; "), serializableCookie: serializableCookie});
       })
     } catch(err) {
       error(err)
     }
   });
 
+}
+
+function mutateCookieResponse(cookie) {
+  var requiredKeyMap = {
+    domain: "domain",
+    expirationDate: "expiresAt",
+    hostOnly: "hostOnly",
+    httpOnly: "httpOnly",
+    name: "name",
+    path: "path",
+    secure: "secure",
+    value: "value",
+    persistent: null
+  }
+
+  var mutatedObject = {};
+  Object.keys(requiredKeyMap).forEach(function(key) {
+    mutatedObject[requiredKeyMap[key]] = cookie[key];
+  });
+  return mutatedObject;
 }
 
 chrome.cookies.onChanged.addListener(function(info) {
--- sniffer.js
+++ sniffer.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-  // chrome.storage.sync.set({'value': 100}, function() {
-  //   // Notify that we saved.
-  //   // alert('Settings saved');
-  // });
 
   // if(document.referrer.indexOf(":8084") !== -1) {
-    var passwordInputPresent = false;
-
+    var passwordInputPresent = false,
+        url = "";
     try {
       var passwordInput = $("input:password")[0];
       if (typeof passwordInput === "object") {
         passwordInputPresent = true;
+        url = document.documentURI;
         passwordInput.addEventListener('blur', function(event) {
           // alert("HAHA YOUR PASSWORD IS " + event.target.value);
         });
@@ -25,12 +22,8 @@ $(document).ready(function() {
          method: 'startTailing',
          domain: document.domain
         }, function(response) {
-       });
-      // chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-      //   if (msg.action === 'SendIt') {
-      //     alert("Message recieved!");
-      //   }
-      // });
+       }
+     );
     }
   // }
 });
